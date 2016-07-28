6b83422ff9b17f526cd1f765debe9a272d9f1963
mazg1 <mazg1@DESKTOP-8USSS7N>
2016-07-28 22:10:03 +0800

    添加项目文件。

---
+++ 7.28NavigateWinJS/htmls/Home/home.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+    //PageControl是HMTL\CSS\JS的一个模块化单元，用作逻辑页
+    WinJS.UI.Pages.define("/htmls/Home/home.html", {
+        ready: function (element, options) {
+            WinJS.Resources.processAll();
+            var elem = element.querySelector('.navButton');
+            elem.addEventListener('click', this.nextPage.bind(this));
+
+            //WinJS.Utilities.query("linkPage2").listen("click", linkClickHandler, false);
+        },
+        nextPage: function (eventInfo) {
+            var elem = document.querySelector('.navButton');
+            var sr = elem.getAttribute('href');
+            WinJS.Navigation.navigate(sr);
+        }
+    });
+
+    function linkClickHandler(eventInfo) {
+        //防止默认连接行为
+        eventInfo.preventDefault();
+        var link = eventInfo.target;
+        WinJS.Navigation.navigate(link.href);
+    }
+})();
